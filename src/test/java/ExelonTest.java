a6f1bc16c4f33db90bdaab1b8b5fcaef0f4ef3f1
Irina Behrend <irinabnd@gmail.com>
2021-10-17 21:11:51 -0500

    code updates to the test file

---
+++ src/test/java/ExelonTest.java
@@ -0,0 +1,54 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class ExelonTest {
+
+    private WebDriver driver;
+    String url = "https://www.exeloncorp.com/";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void setDown() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void firstTest() throws InterruptedException {
+
+        String expectedResult = "https://www.exeloncorp.com/grid";
+
+        driver.get(url);
+        Thread.sleep(5000);
+
+        WebElement gridTest = driver.findElement(By.xpath("//div[@id='exelon-main-nav']" +
+                "/ul[@class='nav navbar-nav navbar-right']/li/a[@href='/grid']"));
+        gridTest.click();
+        WebElement innovationTest = driver.findElement(By.xpath("//form[@id='aspnetForm']/div//div//div//div" +
+                "[@id='WebPartWPQ1']/div//ul[@id='topNav']/li[@class='term-nav active']/a[\"Innovation\"]"));
+        innovationTest.click();
+
+        String actualResult = driver.getCurrentUrl();
+        Assert.assertEquals(actualResult, expectedResult);
+    }
+
+}
+
+/*
+
+WebElement storkTest = driver.findElement(By.xpath("//form[@id='aspnetForm']/div[@id='s4-workspace']" +
+                "//div//div//div[@id='WebPartWPQ1']//div//div[@class='tile']/img[\"true\"]"));
+        storkTest.isDisplayed();
+ */
---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,43 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    private static WebDriver driver;
+
+    @BeforeMethod
+    public void before() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void after() {
+        driver.quit();
+    }
+
+    protected WebDriver getDriver() {
+        return driver;
+    }
+
+    protected WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), 10);
+    }
+
+    public static void scroll(WebDriver driver, WebElement element) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].scrollIntoView();", element);
+    }
+}
